0fc6f41a2726c6b994cd4c4c381960b9968bd234
easy.sugar <suprunov.vladyslav@gmail.com>
2022-07-31 21:13:56 +0300

    update settings paths

--- settings/quiplash.py
+++ settings/quiplash.py
@@ -1,6 +1,11 @@
+# original
 PATH = r'C:\Program Files (x86)\Steam\steamapps\common\Quiplash 2 InterLASHional'
 PATH_QUESTIONS = PATH + r'\content\QuiplashQuestion'
 PATH_QUESTIONS_JSON = PATH + r'\content\QuiplashQuestion.jet'
-PATH_RELEASE = r'C:\Users\админ\Desktop\Jackbox\Quiplash 2\jackbox-quiplash2-ua'
-PATH_BACKUP = r'C:\Users\админ\Desktop\Jackbox\Quiplash 2\backup'
+
+# release
+PATH_RELEASE = r'~\Desktop\Jackbox\Quiplash 2\jackbox-quiplash2-ua'
+
+# backup
+PATH_BACKUP = r'~\Desktop\Jackbox\Quiplash 2\backup'
 PATH_BACKUP_QUESTIONS_JSON = PATH_BACKUP + r'\content\QuiplashQuestion.jet'
--- settings/tmp2.py
+++ settings/tmp2.py
@@ -1,3 +1,7 @@
+# original
 PATH = r'C:\Program Files (x86)\Steam\steamapps\common\The Jackbox Party Starter\games\triviadeath2'
 PATH_QUIPLASH = PATH + r'\content\en\QuiplashContent.jet'
 PATH_DICTATION = PATH + r'\content\en\TDDictation.jet'
+
+# release
+PATH_RELEASE = r'~\Desktop\Jackbox\starter-pack\jackbox-starter-pack-ua'
