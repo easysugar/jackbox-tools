40d21f12e765de83f60ffa483295a6bb3c6bc45f
easy.sugar <suprunov.vladyslav@gmail.com>
2022-07-31 21:10:30 +0300

    add settings for games

---
+++ settings/quiplash.py
@@ -0,0 +1,6 @@
+PATH = r'C:\Program Files (x86)\Steam\steamapps\common\Quiplash 2 InterLASHional'
+PATH_QUESTIONS = PATH + r'\content\QuiplashQuestion'
+PATH_QUESTIONS_JSON = PATH + r'\content\QuiplashQuestion.jet'
+PATH_RELEASE = r'C:\Users\админ\Desktop\Jackbox\Quiplash 2\jackbox-quiplash2-ua'
+PATH_BACKUP = r'C:\Users\админ\Desktop\Jackbox\Quiplash 2\backup'
+PATH_BACKUP_QUESTIONS_JSON = PATH_BACKUP + r'\content\QuiplashQuestion.jet'
---
+++ settings/tmp2.py
@@ -0,0 +1,3 @@
+PATH = r'C:\Program Files (x86)\Steam\steamapps\common\The Jackbox Party Starter\games\triviadeath2'
+PATH_QUIPLASH = PATH + r'\content\en\QuiplashContent.jet'
+PATH_DICTATION = PATH + r'\content\en\TDDictation.jet'
