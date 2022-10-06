530806ea5bea0a21ac1aaef1ae48557550fce72a
easy.sugar <suprunov.vladyslav@gmail.com>
2022-10-06 23:13:30 +0300

    add guesspionage

---
+++ settings/guesspionage.py
@@ -0,0 +1,6 @@
+PATH = r'C:\Program Files (x86)\Steam\steamapps\common\The Jackbox Party Pack 3\games\PollPosition'
+PATH_QUESTIONS = PATH + r'\PollPositionQuestions.jet'
+PATH_BONUS_QUESTIONS = PATH + r'\PollPositionBonusQuestions.jet'
+
+# local
+PATH_EXPANDED = 'data/jpp3/pollposition/swf/PollPosition_GameMain_Expanded.json'
