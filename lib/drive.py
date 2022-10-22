e0e0b74b8d8a590a9801574e74e5190f49792cc6
easy.sugar <suprunov.vladyslav@gmail.com>
2022-10-22 16:17:24 +0300

    update drive module

--- lib/drive.py
+++ lib/drive.py
@@ -1,4 +1,5 @@
 import os
+from typing import Dict
 
 import tqdm
 from pydrive.auth import GoogleAuth
@@ -13,12 +14,15 @@ class Drive:
         self.gauth = GoogleAuth()
         self.drive = GoogleDrive(self.gauth)
 
-    def copy_audio_files_to_drive(self, game: str, path_subtitles: str, path_audio_folder: str):
-        subtitles = read_json(path_subtitles)
+    def copy_audio_files_to_drive(self, game: str):
+        subtitles = read_json(SUBTITLES[game])
         ids = list(subtitles)
         for _id in tqdm.tqdm(ids):
-            file = os.path.join(path_audio_folder, f'{_id}.ogg')
-            gfile = self.drive.CreateFile({'parents': [{'id': FOLDERS[game]}], 'title': f'{_id}.ogg'})
-            # Read file and set it as the content of this instance.
+            file = os.path.join(MEDIA_FOLDER[game], f'{_id}.ogg')
+            gfile = self.drive.CreateFile({'parents': [{'id': DRIVE_FOLDER[game]}], 'title': f'{_id}.ogg'})
             gfile.SetContentFile(file)
-            gfile.Upload()  # Upload the file.
+            gfile.Upload()
+
+    def get_files_links(self, game) -> Dict[str, str]:
+        file_list = self.drive.ListFile({'q': "'{}' in parents and trashed=false".format(DRIVE_FOLDER[game])}).GetList()
+        return {file['originalFilename'].split('.')[0]: file['alternateLink'] for file in file_list}
--- settings/drive.py
+++ settings/drive.py
@@ -1,6 +1,15 @@
 TOKEN = ''
 
 # folder id from google drive
-FOLDERS = {
-    'teeko': '',
+DRIVE_FOLDER = {
+    'teeko': '16uM0K69keNS9ZjltEGAQjwLWdfnhGf2E',
+    'drawful2': '1T5SRc_cpajzrG9eq1FOQ0S3TQYD9UB-r',
+}
+MEDIA_FOLDER = {
+    'teeko': r'C:\Program Files (x86)\Steam\steamapps\common\The Jackbox Party Starter\games\AwShirt\TalkshowExport\project\media',
+    'drawful2': r'C:\Program Files (x86)\Steam\steamapps\common\Drawful 2\TalkshowExport\project\media',
+}
+SUBTITLES = {
+    'teeko': '../data/tjsp/teeko/translated/audio_subtitles.json',
+    'drawful2': '../data/standalone/drawful2/encoded/audio_subtitles.json',
 }
