889ce54a2cbcc275f751a8e8dc24f2f01243694e
vladyslav.suprunov <vladyslav.suprunov@pm.bet>
2022-10-31 19:44:45 +0200

    add script for get top contributors

---
+++ scripts/get_top_contributors.py
@@ -0,0 +1,13 @@
+import json
+from datetime import datetime
+
+from lib.crowdin import Crowdin
+
+DATE = datetime(2022, 10, 1)
+SAVE_PATH = 'contributors.json'
+
+if __name__ == '__main__':
+    c = Crowdin(DATE)
+    contributors = c.get_top_contributors_usernames()
+    with open(SAVE_PATH, 'w') as f:
+        json.dump(contributors, f)
