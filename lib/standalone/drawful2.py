288d8fa7e288c40bdb7d5dbbee24adc748c8c973
easy.sugar <suprunov.vladyslav@gmail.com>
2022-11-04 02:56:59 +0200

    update drawful paths

--- lib/standalone/drawful2.py
+++ lib/standalone/drawful2.py
@@ -5,23 +5,25 @@ from settings.drawful2 import *
 
 
 class Drawful2(Game):
-    @encode_mapping('data/drawful2/encoded/expanded.json', 'data/drawful2/encoded/audio_subtitles.json')
+    folder = 'data/standalone/drawful2/encoded/'
+
+    @encode_mapping(folder + 'expanded.json', folder + 'audio_subtitles.json')
     def encode_audio_subtitles(self, obj: dict):
         return {
             v['id']: v['text'].replace('[category=host]', '').strip() for c in obj for v in c['versions']
             if c['type'] == 'A' and v['tags'] == 'en' and not v['text'].endswith('[Unsubtitled]')
         }
 
-    @encode_mapping('data/drawful2/encoded/expanded.json', 'data/drawful2/encoded/text_subtitles.json')
+    @encode_mapping(folder + 'expanded.json', folder + 'text_subtitles.json')
     def encode_text_subtitles(self, obj: dict):
         return {v['id']: v['text'] for c in obj for v in c['versions'] if c['type'] == 'T'
                 and v['locale'] == 'en' and not v['text'].startswith('SFX/')}
 
-    @encode_mapping(PATH_DECOY, 'data/drawful2/encoded/decoy.json')
+    @encode_mapping(PATH_DECOY, folder + 'decoy.json')
     def encode_decoy(self, obj: dict):
         return {c['id']: c['text'].strip() for c in obj['content']}
 
-    @encode_mapping(PATH_PROMPT, 'data/drawful2/encoded/prompt.json')
+    @encode_mapping(PATH_PROMPT, folder + 'prompt.json')
     def encode_prompt(self, obj: dict):
         result = {}
         for c in obj['content']:
