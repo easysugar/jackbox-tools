87f415cfbb13e146d4a98abc9c744dad01396c64
easy.sugar <suprunov.vladyslav@gmail.com>
2022-11-04 02:53:59 +0200

    update encode_prompt for drawful

--- lib/standalone/drawful2.py
+++ lib/standalone/drawful2.py
@@ -1,4 +1,6 @@
-from lib.game import Game, encode_mapping
+import os
+
+from lib.game import Game, encode_mapping, read_json
 from settings.drawful2 import *
 
 
@@ -21,4 +23,18 @@ class Drawful2(Game):
 
     @encode_mapping(PATH_PROMPT, 'data/drawful2/encoded/prompt.json')
     def encode_prompt(self, obj: dict):
-        return {c['id']: c['category'].strip() for c in obj['content']}
+        result = {}
+        for c in obj['content']:
+            cid = str(c['id'])
+            path = os.path.join(PATH_PROMPT_DIR, cid, 'data.jet')
+            x = read_json(path)
+            x = {_['n']: _ for _ in x['fields']}
+            text = x['QuestionText']['v'].strip()
+            # alternate = [_.strip() for _ in x['AlternateSpellings']['v'].split('|')]
+            audio = None if 'JokeAudio' not in x else x['JokeAudio']['s'].strip()
+            body = text
+            if audio:
+                assert '\n' not in audio
+                body += '\n' + audio
+            result[cid] = body
+        return result
