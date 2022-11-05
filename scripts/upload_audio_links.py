afd1f9733a1b53214800af57b56f413f1d982e05
easy.sugar <suprunov.vladyslav@gmail.com>
2022-11-05 23:10:35 +0200

    update script upload audio links

--- scripts/upload_audio_links.py
+++ scripts/upload_audio_links.py
@@ -3,10 +3,11 @@ from lib.drive import Drive
 
 GAME = 'drawful2'
 PROJECT = 'Jackbox UA'
+AUDIO_PATH = '/Drawful2/audio_subtitles.json'
 
 if __name__ == '__main__':
     d = Drive()
     d.copy_audio_files_to_drive(GAME)
     links = d.get_files_links(GAME)
     c = Crowdin()
-    c.publish_audio_links(PROJECT_LIST[PROJECT], links)
+    c.publish_audio_links(PROJECT_LIST[PROJECT], links, AUDIO_PATH)
