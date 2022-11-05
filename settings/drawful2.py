b74329b7f4451ac23bbeb17130699b117e3ba3b5
easy.sugar <suprunov.vladyslav@gmail.com>
2022-11-06 00:39:19 +0200

    update drawful2 settings

--- settings/drawful2.py
+++ settings/drawful2.py
@@ -2,3 +2,4 @@
 PATH = r'C:\Program Files (x86)\Steam\steamapps\common\Drawful 2'
 PATH_DECOY = PATH + r'\content\Drawful2Decoy.jet'
 PATH_PROMPT = PATH + r'\content\en\Drawful2Prompt.jet'
+PATH_PROMPT_DIR = PATH + r'\content\en\Drawful2Prompt'
