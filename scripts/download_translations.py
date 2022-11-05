95599dedaf4df40013ce402b34daf415ab566798
easy.sugar <suprunov.vladyslav@gmail.com>
2022-11-05 23:08:23 +0200

    set path for build

--- scripts/download_translations.py
+++ scripts/download_translations.py
@@ -1,8 +1,10 @@
 from lib.crowdin import Crowdin, PROJECT_LIST
 
 PROJECT = 'Jackbox Starter Pack UA'
+PATH_BUILD = '../build.zip'
+PATH_FOLDER = '../build'
 
 if __name__ == '__main__':
     c = Crowdin()
-    c.download_last_build(PROJECT_LIST[PROJECT])
-    c.unzip_build()
+    c.download_last_build(PROJECT_LIST[PROJECT], PATH_BUILD)
+    c.unzip_build(PATH_BUILD, PATH_FOLDER)
