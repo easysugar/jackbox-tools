e93840f58f1a18e7c0df5c4b2fceb80d3154817d
easy.sugar <suprunov.vladyslav@gmail.com>
2022-11-19 23:20:46 +0200

    update guesspionage

--- lib/pp3/guesspionage.py
+++ lib/pp3/guesspionage.py
@@ -1,11 +1,13 @@
 import re
 
-from lib.game import Game, decode_mapping
+from lib.game import Game, decode_mapping, read_from_folder
 from settings.guesspionage import *
 
 
 class Guesspionage(Game):
-    @decode_mapping(PATH_EXPANDED, 'data/pp3/pollposition/encoded/audio_subtitles.json')
+    folder = '../data/pp3/pollposition/encoded/'
+
+    @decode_mapping(PATH_EXPANDED, folder + 'audio_subtitles.json')
     def encode_audio_subtitles(self, obj: dict):
         sfx = re.compile(r'\[category=(sfx|music)]$|^\w+\d:\n|^PP_\w+|^Radio Play short |^Radio Play |Back button pressed')
         return {
@@ -14,3 +16,26 @@ class Guesspionage(Game):
             for v in c['versions']
             if c['type'] == 'A' and not sfx.search(v['text'])
         }
+
+    @decode_mapping(PATH_LEADERBOARDS, folder + 'leaderboards.json')
+    def encode_leaderboards(self, obj):
+        return {
+            'columns': {i['id']: i['name'] for i in obj['columns']},
+            'views': {i['id']: {'name': i['name'], 'description': i['description']} for i in obj['views']},
+        }
+
+    @decode_mapping(PATH_BONUS_QUESTIONS, folder + 'bonus_questions.json')
+    def encode_bonus_questions(self, obj):
+        result = {}
+        for c in obj['content']:
+            cid = str(c['id'])
+            x = read_from_folder(cid, PATH_BONUS_QUESTIONS_DIR)
+            body = {x['QText']['v']: {
+                "question": x['PollQ']['v'],
+                'text': x['QText']['v'],
+                'choices': [x['Choice%d' % i]['v'] for i in range(9)],
+                'data_mode': x['DataMode']['v'],
+                'exp_results': x['ExpResults']['v'],
+            }}
+            result[cid] = body
+        return result
--- settings/guesspionage.py
+++ settings/guesspionage.py
@@ -1,8 +1,8 @@
 PATH = r'C:\Program Files (x86)\Steam\steamapps\common\The Jackbox Party Pack 3\games\PollPosition'
-PATH_QUESTIONS = PATH + r'\PollPositionQuestions.jet'
-PATH_BONUS_QUESTIONS = PATH + r'\PollPositionBonusQuestions.jet'
+PATH_QUESTIONS = PATH + r'\content\PollPositionQuestions.jet'
+PATH_BONUS_QUESTIONS = PATH + r'\content\PollPositionBonusQuestions.jet'
 PATH_LEADERBOARDS = PATH + r'\leaderboards.jet'
 PATH_BONUS_QUESTIONS_DIR = PATH + r'\content\PollPositionBonusQuestions'
 
 # local
-PATH_EXPANDED = 'data/pp3/pollposition/swf/PollPosition_GameMain_Expanded.json'
+PATH_EXPANDED = '../data/pp3/pollposition/swf/PollPosition_GameMain_Expanded.json'
