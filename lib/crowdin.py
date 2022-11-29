48a190c7a21ca236a9b2595c8444bed083852d82
vladyslav.suprunov <vladyslav.suprunov@pm.bet>
2022-11-29 22:48:34 +0200

    :tada: update crowdin and add script

--- lib/crowdin.py
+++ lib/crowdin.py
@@ -1,7 +1,7 @@
 import zipfile
 from collections import Counter
 from datetime import datetime, timedelta
-from typing import Callable, List, Dict
+from typing import Callable, List, Dict, Set
 
 import pytz
 import requests
@@ -32,7 +32,12 @@ class Crowdin:
             offset += 500
             for t in data:
                 t = t['data']
-                res.append({'id': t['user']['id'], 'createdAt': t['createdAt'].replace(tzinfo=utc)})
+                res.append({
+                    'id': t['user']['id'],
+                    'username': t['user'].get('username') or t['user'].get('fullName'),
+                    'createdAt': t['createdAt'].replace(tzinfo=utc),
+                    'string_id': t['stringId'],
+                })
         return res
 
     def get_project_files(self, project_id):
@@ -117,9 +122,9 @@ class Crowdin:
 
     def get_top_contributors_last_time(self, list_projects: List[int]) -> Counter:
         return (
-            self.get_total_translators(list_projects) +
-            self.get_total_approvers(list_projects) +
-            self.get_total_commenters(list_projects)
+            self.get_total_translators(list_projects)
+            # self.get_total_approvers(list_projects) +
+            # self.get_total_commenters(list_projects)
         )
 
     def get_users_info(self, users: List[int], list_projects: List[int]) -> dict:
@@ -159,7 +164,9 @@ class Crowdin:
         with zipfile.ZipFile(path_build, 'r') as zip_ref:
             zip_ref.extractall(path_folder)
 
-    def get_strings_ids(self, project_id: int, file_id: int) -> Dict[str, int]:
+    def get_strings_ids_map(self, project_id: int, file_id: int = None, file_path: str = None) -> Dict[str, int]:
+        if file_id:
+            file_id = [f['data']['id'] for f in self.client.source_files.list_files(project_id)['data'] if f['data']['path'] == file_path][0]
         result = {}
         offset = 0
         while True:
@@ -173,10 +180,29 @@ class Crowdin:
                 result[_id] = s['data']['id']
             offset += 500
 
+    def get_string_ids(self, project_id: int, file_id: int) -> Set[int]:
+        result = set()
+        offset = 0
+        while True:
+            batch = self.client.source_strings.list_strings(project_id, file_id, limit=500, offset=offset)
+            if not batch or not batch['data']:
+                return result
+            for s in batch['data']:
+                result.add(s['data']['id'])
+            offset += 500
+
+    def get_strings_ids_by_folder(self, project_id: int, path: str) -> Set[int]:
+        all_files = self.client.source_files.list_files(project_id)['data']
+        string_ids = set()
+        for f in all_files:
+            if f['data']['path'].startswith(path):
+                string_ids |= self.get_string_ids(project_id, f['data']['id'])
+        return string_ids
+
     def publish_audio_links(self, project_id: int, links: dict, crowdin_audio_path: str):
         list_files = self.client.source_files.list_files(project_id, limit=500)
         file_id = [f['data']['id'] for f in list_files['data'] if f['data']['path'] == crowdin_audio_path][0]
-        strings = self.get_strings_ids(project_id, file_id)
+        strings = self.get_strings_ids_map(project_id, file_id)
         for audio_id in tqdm.tqdm(list(strings)):
             self.publish_comment(project_id, strings[audio_id], links[audio_id])
 
@@ -192,3 +218,13 @@ class Crowdin:
                 "issueType": None,
             },
         )
+
+    def get_strings_texts(self, project_id: int, string_ids: List[int]):
+        return {x: self.client.source_strings.get_string(project_id, x)['data']['text'] for x in string_ids}
+
+    def get_approved_translations(self, project_id: int, string_ids: List[int]):
+        return {
+            sid: self.client.string_translations.get_translation(project_id, translationId=x['data']['translationId'])['data']['text']
+            for sid in string_ids
+            for x in self.client.string_translations.list_translation_approvals(project_id, stringId=sid, languageId='uk')['data']
+        }
