b10b61ab3370e195fd8da3444b1e0d7002e98915
vladyslav.suprunov <vladyslav.suprunov@pm.bet>
2022-11-29 23:37:34 +0200

    add print contributors names

--- scripts/get_top_contributors.py
+++ scripts/get_top_contributors.py
@@ -9,5 +9,6 @@ SAVE_PATH = 'contributors.json'
 if __name__ == '__main__':
     c = Crowdin(DATE)
     contributors = c.get_top_contributors_usernames()
+    print(*[x['name'] for x in contributors], sep='\n')
     with open(SAVE_PATH, 'w') as f:
         json.dump(contributors, f, indent=4)
