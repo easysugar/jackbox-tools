544dac9e2a41b2a784dc3bda707410fee3e43892
vladyslav.suprunov <vladyslav.suprunov@pm.bet>
2022-11-30 19:42:54 +0200

    update crowdin settings

--- settings/crowdin.py
+++ settings/crowdin.py
@@ -1,2 +1,11 @@
 TOKEN = "changeme"
-PROJECT_LIST = [528690, 528716]
+PROJECT_LIST = {'Jackbox UA': 528690, 'Jackbox Starter Pack': 528716}
+
+GAME_PATHS = {
+    'drawful2': '/Drawful2',
+    'quiplash2': '/Quiplash2',
+    'fakin': "/JPP3/Fakin' It",
+    'guesspionage': '/JPP3/Guesspionage',
+    'tmp2': '/TMP2',
+    'teeko': '/TeeKO',
+}
