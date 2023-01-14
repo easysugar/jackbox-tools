bca5f86928d667872c978821d0463fb5255aa675
easy.sugar <suprunov.vladyslav@gmail.com>
2023-01-14 23:20:44 +0200

    add prints

--- scripts/download_translations.py
+++ scripts/download_translations.py
@@ -2,14 +2,16 @@ import time
 
 from lib.crowdin import Crowdin, PROJECT_LIST
 
-PROJECT = 'starter'
-# PROJECT = 'general'
+# PROJECT = 'starter'
+PROJECT = 'general'
 PATH_BUILD = '../build.zip'
 PATH_FOLDER = '../build'
 
 if __name__ == '__main__':
     c = Crowdin()
+    print('Creating build...')
     c.create_build(PROJECT_LIST[PROJECT])
     time.sleep(10)  # wait until project is building
+    print('Downloading build...')
     c.download_last_build(PROJECT_LIST[PROJECT], PATH_BUILD)
     c.unzip_build(PATH_BUILD, PATH_FOLDER)
