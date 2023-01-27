37b88028f729b8d888df9260f6d7163694526b9e
easy.sugar <suprunov.vladyslav@gmail.com>
2023-01-27 23:29:31 +0200

    upload links to drive via walk by dirs

--- lib/drive.py
+++ lib/drive.py
@@ -14,14 +14,26 @@ class Drive:
         self.gauth = GoogleAuth()
         self.drive = GoogleDrive(self.gauth)
 
-    def copy_audio_files_to_drive(self, game: str):
+    def copy_to_drive(self, folder, filepath, filename):
+        gfile = self.drive.CreateFile({'parents': [{'id': folder}], 'title': filename})
+        gfile.SetContentFile(filepath)
+        gfile.Upload()
+
+    def copy_audio_subtitles_to_drive(self, game: str):
         subtitles = read_json(SUBTITLES[game])
         ids = list(subtitles)
         for _id in tqdm.tqdm(ids):
             file = os.path.join(MEDIA_FOLDER[game], f'{_id}.ogg')
-            gfile = self.drive.CreateFile({'parents': [{'id': DRIVE_FOLDER[game]}], 'title': f'{_id}.ogg'})
-            gfile.SetContentFile(file)
-            gfile.Upload()
+            self.copy_to_drive(DRIVE_FOLDER[game], file, f'{_id}.ogg')
+
+    def copy_audio_to_drive_by_walk(self, game: str, src: str):
+        for root, _, files in tqdm.tqdm(list(os.walk(src))):
+            files = [f for f in files if f.endswith('.ogg')]
+            assert len(files) <= 1
+            folder = root.split(os.path.sep)[-1]
+            for name in files:
+                filepath = os.path.join(root, name)
+                self.copy_to_drive(DRIVE_FOLDER[game], filepath, f'{folder}.ogg')
 
     def get_files_links(self, game) -> Dict[str, str]:
         file_list = self.drive.ListFile({'q': "'{}' in parents and trashed=false".format(DRIVE_FOLDER[game])}).GetList()
--- scripts/upload_audio_links.py
+++ scripts/upload_audio_links.py
@@ -1,13 +1,15 @@
-from lib.crowdin import Crowdin, PROJECT_LIST
+from lib.common import write_json
 from lib.drive import Drive
 
 GAME = 'drawful2'
 PROJECT = 'Jackbox UA'
 AUDIO_PATH = '/Drawful2/audio_subtitles.json'
+FOLDER_PATH = r'C:\Program Files (x86)\Steam\steamapps\common\Drawful 2\content\en\Drawful2Prompt'
 
 if __name__ == '__main__':
     d = Drive()
-    d.copy_audio_files_to_drive(GAME)
+    d.copy_audio_to_drive_by_walk(GAME, FOLDER_PATH)
     links = d.get_files_links(GAME)
-    c = Crowdin()
-    c.publish_audio_links(PROJECT_LIST[PROJECT], links, AUDIO_PATH)
+    write_json('../data/standalone/drawful2/audio_links.json', links)
+    # c = Crowdin()
+    # c.publish_audio_links(PROJECT_LIST[PROJECT], links, AUDIO_PATH)
