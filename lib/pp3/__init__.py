8c218a5e62a86a1b56bcaf5455ae04fb283b7d93
easy.sugar <suprunov.vladyslav@gmail.com>
2023-01-28 18:40:16 +0200

    add tmp1

--- lib/pp3/__init__.py
+++ lib/pp3/__init__.py
@@ -2,3 +2,4 @@ from .fakin import Fakin
 from .guesspionage import Guesspionage
 from .jpp3 import JPP3
 from .old_teeko import OldTeeKO
+from .tmp import TMP
---
+++ settings/tmp.py
@@ -0,0 +1,5 @@
+# encoded
+PATH = r'C:\Program Files (x86)\Steam\steamapps\common\The Jackbox Party Pack 3\games\TriviaDeath'
+PATH_LEADERBOARDS = PATH + r'\leaderboards.jet'
+PATH_SETTINGS = PATH + r'\settings.jet'
+PATH_QUESTION = PATH + r'\content\TDQuestion.jet'
