e0229eb0c49b4f9bb8bde8d9b67de19beca08151
easysugar <suprunov.vladyslav@gmail.com>
2023-02-18 23:19:43 +0200

    update report generation

--- scripts/get_top_contributors.py
+++ scripts/get_top_contributors.py
@@ -3,7 +3,7 @@ from datetime import datetime
 
 from lib.crowdin import Crowdin
 
-DATE = datetime(2023, 1, 1)
+DATE = datetime(2023, 2, 1)
 SAVE_PATH = 'contributors.json'
 
 if __name__ == '__main__':
@@ -11,4 +11,4 @@ if __name__ == '__main__':
     contributors = c.get_top_contributors_usernames()
     print(*[x['name'] for x in contributors], sep='\n')
     with open(SAVE_PATH, 'w') as f:
-        json.dump(contributors, f, indent=4)
+        json.dump(contributors, f, indent=4, ensure_ascii=False)
