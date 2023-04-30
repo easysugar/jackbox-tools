41e9613641f257df7e544c4d8b16ae7160f98e18
easy.sugar <suprunov.vladyslav@gmail.com>
2023-04-30 18:40:14 +0300

    update creating audio table

--- settings/drive.py
+++ settings/drive.py
@@ -4,12 +4,21 @@ TOKEN = ''
 DRIVE_FOLDER = {
     'teeko': '16uM0K69keNS9ZjltEGAQjwLWdfnhGf2E',
     'drawful2': '1T5SRc_cpajzrG9eq1FOQ0S3TQYD9UB-r',
+    'quiplash2': '1VGOKQrGlZn8tjliolfDVJfsMk4UQIoeH',
+    'guesspionage': '1ejoXJnO1GFMElG7mXVHPc5mZwcRHoIwh',
+    'fakin': '1zUoCoZQB83vKeWO3GghCmdr4rPCZJ3UG',
 }
 MEDIA_FOLDER = {
     'teeko': r'C:\Program Files (x86)\Steam\steamapps\common\The Jackbox Party Starter\games\AwShirt\TalkshowExport\project\media',
     'drawful2': r'C:\Program Files (x86)\Steam\steamapps\common\Drawful 2\TalkshowExport\project\media',
+    'quiplash2': r'C:\Program Files (x86)\Steam\steamapps\common\Quiplash 2 InterLASHional\TalkshowExport\project\media',
+    'guesspionage': r'C:\Program Files (x86)\Steam\steamapps\common\The Jackbox Party Pack 3\games\PollPosition\TalkshowExport\project\media',
+    'fakin': r'C:\Program Files (x86)\Steam\steamapps\common\The Jackbox Party Pack 3\games\FakinIt\TalkshowExport\project\media',
 }
 SUBTITLES = {
     'teeko': '../data/tjsp/teeko/translated/audio_subtitles.json',
     'drawful2': '../data/standalone/drawful2/encoded/audio_subtitles.json',
+    'quiplash2': '../data/standalone/quiplash2/encoded/audio_subtitles.json',
+    'guesspionage': '../data/pp3/pollposition/encoded/audio_subtitles.json',
+    'fakin': '../data/pp3/fakin/swf/audio_subtitles.json',
 }
