b265e9240524f9c5f87ccdb2f8e9c60d931cefb3
easysugar <suprunov.vladyslav@gmail.com>
2023-05-30 22:44:20 +0300

    add decoders to monsters

--- lib/pp4/monster.py
+++ lib/pp4/monster.py
@@ -4,6 +4,8 @@ from settings.monster import *
 
 class Monster(Game):
     folder = '../data/pp4/monster/encoded/'
+    folder_swf = '../data/pp4/monster/swf/'
+    build = '../build/uk/JPP4/MSM/'
 
     def add_plural234(self, obj):
         if isinstance(obj, dict):
@@ -19,6 +21,18 @@ class Monster(Game):
                 self.add_plural234(item)
         return obj
 
+    def _decode_plural234(self, obj, trans):
+        if isinstance(obj, dict):
+            for key in list(obj):
+                if isinstance(obj[key], dict):
+                    self._decode_plural234(obj[key], trans[key])
+                if 'plural234' in key or 'multiple234' in key:
+                    obj.pop(key)
+        if isinstance(obj, list):
+            for i, item in enumerate(obj):
+                self._decode_plural234(item, trans[i])
+        return obj
+
     @decode_mapping(PATH_MONSTER, folder + 'monster.json')
     def encode_monster(self, obj):
         skip = ['id', 'frame', 'role', 'minimumPlayers', 'behaviorClass']
@@ -27,13 +41,36 @@ class Monster(Game):
             for c in obj['content']
         })
 
+    @decode_mapping(PATH_MONSTER, build + 'monster.json', PATH_MONSTER)
+    def decode_monster(self, obj, trans):
+        skip = ['id', 'frame', 'role', 'minimumPlayers', 'behaviorClass']
+        trans = self._decode_plural234({
+            str(c['id']): {k: v for k, v in c.items() if k not in skip}
+            for c in obj['content']
+        }, trans)
+        for c in obj['content']:
+            c.update(trans[str(c['id'])])
+        return obj
+
     @decode_mapping(PATH_AUDIENCE_ANSWER, folder + 'audience_answer.json')
     def encode_audience_answer(self, obj):
         return {c['id']: c['answer'] for c in obj['content']}
 
+    @decode_mapping(PATH_AUDIENCE_ANSWER, build + 'audience_answer.json', PATH_AUDIENCE_ANSWER)
+    def decode_audience_answer(self, obj, trans):
+        for c in obj['content']:
+            c['answer'] = trans[str(c['id'])]
+        return obj
+
     @decode_mapping(PATH_AUDIENCE_QUESTION, folder + 'audience_question.json')
     def encode_audience_question(self, obj):
         return {c['id']: c['question'] for c in obj['content']}
+
+    @decode_mapping(PATH_AUDIENCE_QUESTION, build + 'audience_question.json', PATH_AUDIENCE_QUESTION)
+    def decode_audience_answer(self, obj, trans):
+        for c in obj['content']:
+            c['question'] = trans[str(c['id'])]
+        return obj
 
     @decode_mapping(PATH_NPC, folder + 'npc.json')
     def encode_npc(self, obj):
@@ -43,18 +80,57 @@ class Monster(Game):
             for c in obj['content']
         })
 
+    @decode_mapping(PATH_NPC, build + 'npc.json', PATH_NPC)
+    def decode_npc(self, obj, trans):
+        skip = ['id', 'frame', 'role', 'minimumPlayers', 'behaviorClass']
+        trans = self._decode_plural234({
+            str(c['id']): {k: v for k, v in c.items() if k not in skip}
+            for c in obj['content']
+        }, trans)
+        for c in obj['content']:
+            c.update(trans[str(c['id'])])
+        return obj
+
     @decode_mapping(PATH_NPC_ANSWER, folder + 'npc_answer.json')
     def encode_npc_answer(self, obj):
         return {c['id']: c['answer'] for c in obj['content']}
+
+    @decode_mapping(PATH_NPC_ANSWER, build + 'npc_answer.json', PATH_NPC_ANSWER)
+    def decode_npc_answer(self, obj, trans):
+        for c in obj['content']:
+            c['answer'] = trans[str(c['id'])]
+        return obj
 
     @decode_mapping(PATH_SECRET_WORDS, folder + 'secret_words.json')
     def encode_secret_words(self, obj):
         return {c['id']: c['word'] for c in obj['content']}
 
+    @decode_mapping(PATH_SECRET_WORDS, build + 'secret_words.json', PATH_SECRET_WORDS)
+    def decode_secret_words(self, obj, trans):
+        for c in obj['content']:
+            c['word'] = trans[str(c['id'])]
+        return obj
+
     @decode_mapping(PATH_SPONSORS, folder + 'sponsors.json')
     def encode_sponsor(self, obj):
         return {c['id']: c['sponsor'] for c in obj['content']}
 
+    @decode_mapping(PATH_SPONSORS, build + 'sponsors.json', PATH_SPONSORS)
+    def decode_sponsor(self, obj, trans):
+        for c in obj['content']:
+            c['sponsor'] = trans[str(c['id'])]
+        return obj
+
     @decode_mapping(PATH_EXPANDED, folder + 'text_subtitles.json')
     def encode_text_subtitles(self, obj):
         return self._encode_subtitles(obj, 'T', '')
+
+    def decode_media(self):
+        audio = {}
+        text = self._read_json(self.build + 'text_subtitles.json')
+        self._decode_swf_media(
+            path_media=self.folder_swf + 'dict.txt',
+            path_expanded=PATH_EXPANDED,
+            trans=audio | text,
+            path_save=self.folder_swf + 'translated_dict.txt',
+        )
