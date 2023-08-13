574c5af3155214f99bd82bcd88d4fe7edc3d00e0
easy.sugar <suprunov.vladyslav@gmail.com>
2023-08-14 00:43:14 +0300

    add encoders in tmp

--- lib/pp3/tmp.py
+++ lib/pp3/tmp.py
@@ -1,9 +1,13 @@
+import re
+from collections import defaultdict
+
 from lib.game import Game, encode_mapping
 from settings.tmp import *
 
 
 class TMP(Game):
     folder = '../data/pp3/tmp/encoded/'
+    folder_swf = '../data/pp3/tmp/swf/'
 
     @encode_mapping(PATH_QUESTION, folder + 'questions.json')
     def encode_question(self, obj: dict):
@@ -25,6 +29,25 @@ class TMP(Game):
             )
         return result
 
+    @encode_mapping(PATH_FINAL_ROUND, folder + 'final_questions.json')
+    def encode_final_round(self, obj: dict):
+        res = {}
+        for q in obj['content']:
+            text = q['text']
+            corrects = defaultdict(set)
+            for c in q['choices']:
+                assert set(c) == {'correct', 'easy', 'text'}
+                assert c['correct'] in (True, False)
+                assert c['easy'] in (True, False)
+                assert c['text'].strip()
+                t = c['text'].strip()
+                # assert t not in (corrects[True] | corrects[False]), f'answer {t} duplicated in {q["id"]}'
+                corrects[c['correct']].add(t)
+            for c, answers in corrects.items():
+                text += '\n' + '-+'[c] + '\n' + '\n'.join(answers)
+            res[q['id']] = text
+        return res
+
     @encode_mapping(PATH_LEADERBOARDS, folder + 'leaderboards.json')
     def encode_leaderboards(self, obj):
         return {
@@ -37,3 +60,10 @@ class TMP(Game):
         return {
             i['source']: {'title': i['title'], 'description': i['description']} for i in obj['items']
         }
+
+    @encode_mapping(folder_swf + 'expanded.json', folder + 'text_subtitles.json')
+    def encode_text_subtitles(self, obj):
+        return {v['id']: {'text': v['text'], 'crowdinContext': c.get('crowdinContext', '') + ' [' + v['tags'] + ']'}
+                for c in obj
+                for v in c['versions']
+                if c['type'] == 'T' and not re.fullmatch(r'[a-z]+\d*[a-z]?', v['text'])}
--- settings/tmp.py
+++ settings/tmp.py
@@ -3,3 +3,4 @@ PATH = r'C:\Program Files (x86)\Steam\steamapps\common\The Jackbox Party Pack 3\
 PATH_LEADERBOARDS = PATH + r'\leaderboards.jet'
 PATH_SETTINGS = PATH + r'\settings.jet'
 PATH_QUESTION = PATH + r'\content\TDQuestion.jet'
+PATH_FINAL_ROUND = PATH + r'\content\TDFinalRound.jet'
