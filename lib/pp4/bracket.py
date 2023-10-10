43f174fca0744a75a5825e38f57c349defdbf05d
easy.sugar <suprunov.vladyslav@gmail.com>
2023-10-11 00:07:30 +0300

    add decodes to bracketeering

--- lib/pp4/bracket.py
+++ lib/pp4/bracket.py
@@ -5,6 +5,7 @@ PATH_GAME = r'C:\Program Files (x86)\Steam\steamapps\common\The Jackbox Party Pa
 
 class Bracketeering(Game):
     folder = '../data/pp4/bracket/'
+    build = '../build/uk/JPP4/Bracketeering/'
 
     @decode_mapping(PATH_GAME + r'\content\BRKPrompt.jet', folder + 'prompt.json')
     def encode_prompt(self, obj):
@@ -26,10 +27,30 @@ class Bracketeering(Game):
             res[c['id']] = row
         return res
 
+    @decode_mapping(PATH_GAME + r'\content\BRKPrompt.jet', build + 'prompt.json', PATH_GAME + r'\content\BRKPrompt.jet')
+    def decode_prompt(self, obj, trans):
+        for c in obj['content']:
+            t = trans[str(c['id'])]
+            c['category'] = t['category']['text']
+            c['prompt']['text'] = t['prompt']['text']
+            for twist, t_twist in zip(c['twists'], t['twists']):
+                twist['text'] = t_twist['text']
+            for decoy, t_decoy in zip(c['decoys'], t['decoys']):
+                decoy['text'] = t_decoy['text']
+        return obj
+
     @decode_mapping(folder + 'expanded.json', folder + 'text_subtitles.json')
     def encode_text_subtitles(self, obj: dict):
         return {v['id']: v['text'] for c in obj for v in c['versions'] if c['type'] == 'T'}
 
-    @decode_mapping(folder + 'expanded.json', folder + 'text_subtitles.json')
-    def encode_text_subtitles(self, obj: dict):
-        return {v['id']: v['text'] for c in obj for v in c['versions'] if c['type'] == 'A'}
+    def decode_media(self):
+        text = self._read_json(self.build + 'text_subtitles.json')
+        self._decode_swf_media(
+            path_media=self.folder + 'dict.txt',
+            path_expanded=self.folder + 'expanded.json',
+            trans=text,
+            path_save=self.folder + 'translated_dict.txt',
+        )
+
+    def decode_localization(self):
+        self.update_localization(PATH_GAME + r'\Localization.json', self.build + 'localization.json')
