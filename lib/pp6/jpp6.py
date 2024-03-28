59060cd41b8274a27eba49259c5139a52ed7c5a5
easy.sugar <suprunov.vladyslav@gmail.com>
2024-03-28 23:53:48 +0200

    add old TMP2 from JPP6 decoding

--- lib/pp6/jpp6.py
+++ lib/pp6/jpp6.py
@@ -4,16 +4,17 @@ from lib.game import Game
 from .old_tmp2 import OldTMP2
 
 PATH_GAME = r'C:\Program Files (x86)\Steam\steamapps\common\The Jackbox Party Pack 6'
-PATH_RELEASE = r'C:\Users\админ\Desktop\Jackbox\games\jpp6\jackbox-pack-6'
+PATH_RELEASE = r'C:\Users\админ\Desktop\Jackbox\games\jpp6\jpp6'
+INSTALL_DATE = datetime(2024, 2, 5)
 
 
 class JPP6(Game):
     def decode_all(self):
         OldTMP2().decode_all()
-        self.update_localization(rf'{PATH_GAME}\Localization.json', '../build/uk/JPP3/localization.json')
-        self.update_localization(rf'{PATH_GAME}\games\PartyPack\Localization.json', '../build/uk/JPP3/localization_pack.json')
+        self.update_localization(rf'{PATH_GAME}\Localization.json', '../build/uk/JPP6/localization.json')
+        self.update_localization(rf'{PATH_GAME}\games\PartyPack\Localization.json', '../build/uk/JPP6/localization_pack.json')
 
-    def release(self, start_time: datetime):
+    def release(self):
         self.decode_all()
-        self.copy_to_release(PATH_GAME, PATH_RELEASE, start_time)
-        self.make_archive(PATH_RELEASE)
+        self.copy_to_release(PATH_GAME, PATH_RELEASE, INSTALL_DATE)
+        self.make_archive(PATH_RELEASE, 'JPP6-UA.zip')
--- lib/pp6/old_tmp2.py
+++ lib/pp6/old_tmp2.py
@@ -2,7 +2,7 @@ import re
 from collections import defaultdict
 
 from lib.common import write_json
-from lib.game import Game, decode_mapping, remove_suffix, normalize_text, read_from_folder, write_to_folder
+from lib.game import Game, decode_mapping, remove_suffix, normalize_text, read_from_folder, write_to_folder, replace_suffix
 
 PATH = r'C:\Program Files (x86)\Steam\steamapps\common\The Jackbox Party Pack 6\games\TriviaDeath2'
 OLD_PATH = r'C:\Program Files (x86)\Steam\steamapps\common\The Jackbox Party Starter\games\triviadeath2'
@@ -14,8 +14,9 @@ def transform_tags(s: str):
 
 class OldTMP2(Game):
     folder = '../data/pp6/tmp2/'
-    old_folder = '../data/tjsp/tmp2/encoded/'
+    tjsp_folder = '../data/tjsp/tmp2/encoded/'
     build = '../build/uk/JPP6/TMP2/'
+    tjsp_build = '../build/uk/TMP2/'
 
     @decode_mapping(PATH + r'\Localization.json', OLD_PATH + r'\Localization.json', folder + 'localization.json')
     def encode_localization(self, obj, old_obj):
@@ -48,8 +49,9 @@ class OldTMP2(Game):
         for c in obj['content']:
             try:
                 o = old[c['id']]
-                c['text'] = o['text']
-                c['choices'] = [{'controllerClass': '', 'text': _['text']} | ({'correct': True} if _['correct'] else {}) for _ in o['choices']]
+                c['text'] = transform_tags(o['text'])
+                c['choices'] = [{'controllerClass': '', 'text': transform_tags(_['text'])} | ({'correct': True} if _['correct'] else {})
+                                for _ in o['choices']]
             except KeyError:
                 c['text'], *choices, answer = add[str(c['id'])].split('\n')
                 c['choices'] = [{'controllerClass': '', 'text': _} for _ in choices]
@@ -65,8 +67,85 @@ class OldTMP2(Game):
     def decode_quiplash(self, obj, old):
         old = {int(_['id']): _ for _ in old['content']}
         for c in obj['content']:
-            o = old[c['id']]
-            c['prompt'] = o['prompt']
+            c['prompt'] = transform_tags(old[c['id']]['prompt'].split('\n')[0])
+            o = read_from_folder(c['id'], PATH + r'\content\QuiplashContent')
+            o['PromptText']['v'] = c['prompt']
+            o['PromptAudio']['s'] = c['prompt']
+            write_to_folder(c['id'], PATH + r'\content\QuiplashContent', o)
+        return obj
+
+    @decode_mapping(PATH + r'\content\TDQuestionBomb.jet', OLD_PATH + r'\content\en\TDQuestionBomb.jet', PATH + r'\content\TDQuestionBomb.jet')
+    def decode_bomb(self, obj, old):
+        old = {int(_['id']): _ for _ in old['content']}
+        for c in obj['content']:
+            c['text'] = transform_tags(old[c['id']]['text'])
+            c['choices'] = old[c['id']]['choices']
+            for _ in c['choices']:
+                if _['correct'] is False:
+                    _.pop('correct')
+        return obj
+
+    @decode_mapping(PATH + r'\content\TDQuestionGhost.jet', OLD_PATH + r'\content\en\TDQuestionGhost.jet', PATH + r'\content\TDQuestionGhost.jet')
+    def decode_ghost(self, obj, old):
+        old = {int(_['id']): _ for _ in old['content']}
+        for c in obj['content']:
+            c['text'] = transform_tags(old[c['id']]['text'])
+            for c1, c0 in zip(c['choices'], old[c['id']]['choices']):
+                c1['text'] = c0['text']
+        return obj
+
+    @decode_mapping(PATH + r'\content\TDQuestionHat.jet', OLD_PATH + r'\content\en\TDQuestionHat.jet', PATH + r'\content\TDQuestionHat.jet')
+    def decode_hat(self, obj, old):
+        old = {int(_['id']): _ for _ in old['content']}
+        for c in obj['content']:
+            c['text'] = transform_tags(old[c['id']]['text'])
+            for c1, c0 in zip(c['choices'], old[c['id']]['choices']):
+                c1['text'] = c0['text']
+                if 'correct' in c1:
+                    c1.pop('correct')
+                if c0.get('correct'):
+                    c1['correct'] = True
+        return obj
+
+    @decode_mapping(PATH + r'\content\TDQuestionKnife.jet', OLD_PATH + r'\content\en\TDQuestionKnife.jet', PATH + r'\content\TDQuestionKnife.jet')
+    def decode_knife(self, obj, old):
+        old = {int(_['id']): _ for _ in old['content']}
+        for c in obj['content']:
+            c['text'] = transform_tags(old[c['id']]['text'])
+            for c1, c0 in zip(c['choices'], old[c['id']]['choices']):
+                c1['text'] = c0['text']
+                if 'correct' in c1:
+                    c1.pop('correct')
+                if c0.get('correct'):
+                    c1['correct'] = True
+        return obj
+
+    @decode_mapping(PATH + r'\content\TDQuestionMadness.jet', OLD_PATH + r'\content\en\TDQuestionMadness.jet',
+                    PATH + r'\content\TDQuestionMadness.jet')
+    def decode_madness(self, obj, old):
+        old = {int(_['id']): _ for _ in old['content']}
+        for c in obj['content']:
+            c['text'] = transform_tags(old[c['id']]['text'])
+            for c1, c0 in zip(c['choices'], old[c['id']]['choices']):
+                c1['text'] = c0['text']
+                if 'correct' in c1:
+                    c1.pop('correct')
+                if c0.get('correct'):
+                    c1['correct'] = True
+        return obj
+
+    @decode_mapping(PATH + r'\content\TDQuestionWig.jet', OLD_PATH + r'\content\en\TDQuestionWig.jet',
+                    PATH + r'\content\TDQuestionWig.jet')
+    def decode_madness(self, obj, old):
+        old = {int(_['id']): _ for _ in old['content']}
+        for c in obj['content']:
+            c['text'] = transform_tags(old[c['id']]['text'])
+            for c1, c0 in zip(c['choices'], old[c['id']]['choices']):
+                c1['text'] = c0['text']
+                if 'correct' in c1:
+                    c1.pop('correct')
+                if c0.get('correct'):
+                    c1['correct'] = True
         return obj
 
     @decode_mapping(PATH + r'\content\TDFinalRound.jet', OLD_PATH + r'\content\en\TDFinalRound.jet', PATH + r'\content\TDFinalRound.jet')
@@ -74,8 +153,13 @@ class OldTMP2(Game):
         old = {int(_['id']): _ for _ in old['content']}
         for c in obj['content']:
             o = old[c['id']]
-            c['text'] = o['text']
+            c['text'] = transform_tags(o['text'])
             c['choices'] = o['choices']
+            for _ in c['choices']:
+                _['text'] = transform_tags(_['text'])
+            o = read_from_folder(c['id'], PATH + r'\content\TDFinalRound')
+            o['Q']['s'] = c['text']
+            write_to_folder(c['id'], PATH + r'\content\TDFinalRound', o)
         return obj
 
     @decode_mapping(PATH + r'\content\TDMindMeld.jet', OLD_PATH + r'\content\en\TDMindMeld.jet', PATH + r'\content\TDMindMeld.jet')
@@ -83,8 +167,12 @@ class OldTMP2(Game):
         old = {int(_['id']): _ for _ in old['content']}
         for c in obj['content']:
             o = old[c['id']]
-            c['text'] = o['text']
+            c['text'] = transform_tags(o['text'])
             c['answers'] = o['answers']
+            o = read_from_folder(c['id'], PATH + r'\content\TDMindMeld')
+            o['PromptText']['v'] = c['text']
+            o['PromptAudio']['s'] = c['text']
+            write_to_folder(c['id'], PATH + r'\content\TDMindMeld', o)
         return obj
 
     @decode_mapping(PATH + r'\content\TDMirror.jet', OLD_PATH + r'\content\en\TDMirror.jet', PATH + r'\content\TDMirror.jet')
@@ -110,7 +198,7 @@ class OldTMP2(Game):
             c['text'] = old[c['id']]['text']
         return obj
 
-    @decode_mapping(folder + 'expanded.json', old_folder + 'text_subtitles.json', old_folder + 'EncodedAudio.json', folder + 'media_mapping.json')
+    @decode_mapping(folder + 'expanded.json', tjsp_folder + 'text_subtitles.json', tjsp_folder + 'EncodedAudio.json', folder + 'media_mapping.json')
     def get_media_mappings(self, obj, old_text, old_audio):
         maps = {}
         texts = defaultdict(list)
@@ -129,7 +217,7 @@ class OldTMP2(Game):
                                     v['text'].strip()):
                             continue
                         raise Exception
-                    maps[c['id']] = texts[v['text']].pop(0)
+                    maps[v['id']] = texts[v['text']].pop(0)
             elif c['type'] == 'A':
                 for v in c['versions']:
                     t = normalize_text(v['text'])
@@ -140,3 +228,26 @@ class OldTMP2(Game):
         if additional:
             write_json(self.folder + 'additional_audio.json', additional)
         return maps
+
+    @decode_mapping(folder + 'expanded.json', tjsp_build + 'EncodedAudio.json', tjsp_build + 'in-game/text_subtitles.json', out=False)
+    def decode_media(self, obj, tjsp_audio, tjsp_text):
+        tjsp_text = {k: v for _ in tjsp_text.values() for k, v in _.items()}
+        additional_audio = self._read_json(self.build + 'additional_audio.json')
+        maps = self._read_json(self.folder + 'media_mapping.json')
+        obj = {v['id']: v['text'] for c in obj for v in c['versions']}
+        translations = {}  # version.id -> translated text
+        for pp6_id, tjsp_id in maps.items():
+            if tjsp_id in tjsp_audio:
+                translations[pp6_id] = remove_suffix(tjsp_audio[tjsp_id])
+            else:
+                translations[pp6_id] = remove_suffix(tjsp_text[tjsp_id])
+        for vid in additional_audio:
+            translations[vid] = remove_suffix(additional_audio[vid])
+        additional = {'270628': 'Ви отримаєте підказку, якщо вгадаєте правильну букву'}
+        translations.update(additional)
+        self._decode_swf_media(
+            path_media=self.folder + 'dict.txt',
+            path_expanded=self.folder + 'expanded.json',
+            trans=translations,
+            path_save=self.folder + 'translated_dict.txt',
+        )
