eb406b6e10d6b45c01612dabce6767cb6225f8ff
easysugar <suprunov.vladyslav@gmail.com>
2024-10-28 23:22:26 +0200

    move TMP settings into main module

--- lib/pp3/tmp.py
+++ lib/pp3/tmp.py
@@ -2,7 +2,15 @@ import re
 from collections import defaultdict
 
 from lib.game import Game, encode_mapping, decode_mapping, read_from_folder, write_to_folder
-from settings.tmp import *
+from paths import JPP3_PATH
+
+PATH = JPP3_PATH + r'\games\TriviaDeath'
+PATH_LEADERBOARDS = PATH + r'\leaderboards.jet'
+PATH_SETTINGS = PATH + r'\settings.jet'
+PATH_QUESTION = PATH + r'\content\TDQuestion.jet'
+PATH_FINAL_ROUND = PATH + r'\content\TDFinalRound.jet'
+PATH_WORST_RESPONSE = PATH + r'\content\TDWorstResponse.jet'
+PATH_WORST_DRAWING = PATH + r'\content\TDWorstDrawing.jet'
 
 
 class TMP(Game):
--- settings/tmp.py
+++
@@ -1,8 +0,0 @@
-# encoded
-PATH = r'C:\Program Files (x86)\Steam\steamapps\common\The Jackbox Party Pack 3\games\TriviaDeath'
-PATH_LEADERBOARDS = PATH + r'\leaderboards.jet'
-PATH_SETTINGS = PATH + r'\settings.jet'
-PATH_QUESTION = PATH + r'\content\TDQuestion.jet'
-PATH_FINAL_ROUND = PATH + r'\content\TDFinalRound.jet'
-PATH_WORST_RESPONSE = PATH + r'\content\TDWorstResponse.jet'
-PATH_WORST_DRAWING = PATH + r'\content\TDWorstDrawing.jet'
