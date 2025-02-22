3c8d7ab0ce74238f4a0ba54725f323c62f742cad
easysugar <suprunov.vladyslav@gmail.com>
2025-02-22 19:30:05 +0200

    add audio to split the room

--- lib/pp5/split.py
+++ lib/pp5/split.py
@@ -1,12 +1,26 @@
+import csv
+import os
+
+import pandas as pd
+import tqdm
+
+from lib.audio import get_audio_markers
+from lib.common import copy_file
+from lib.drive import Drive
 from lib.game import Game, decode_mapping, read_from_folder, write_to_folder
 from paths import JPP5_PATH
 
 PATH = JPP5_PATH + r'\games\SplitTheRoom'
+PATH_MEDIA = PATH + r'\TalkshowExport\project\media'
 
 
 class SplitTheRoom(Game):
+    game = PATH
+    drive = '1-7isayk367umf7bz3tYF913DXrowy6o_'
     folder = '../data/pp5/split/'
     build = '../build/uk/JPP5/SplitTheRoom/'
+    audio = r'X:\Jackbox\games\jpp5\split\audio\scenarios'
+    rounds = {1: 'SplitTheRoomShortie', 2: 'SplitTheRoomLater', 3: 'SplitTheRoomFinal'}
 
     @decode_mapping(folder + 'expanded.json', folder + 'text_subtitles.json')
     def encode_text_subtitles(self, obj: dict):
@@ -47,8 +61,7 @@ class SplitTheRoom(Game):
                 res[c['id']]['response'] = {'text': o['ResponseAudio']['s'], 'crowdinContext': context}
         return res
 
-    @staticmethod
-    def _decode_scenario(obj, trans, path_folder: str):
+    def _decode_scenario(self, obj, trans, path_folder: str):
         for c in obj['content']:
             cid = str(c['id'])
             t = trans[cid]
@@ -69,6 +82,10 @@ class SplitTheRoom(Game):
             assert f'ScenarioText{n}' in o and f'ScenarioText{n + 1}' not in o, f'Prompt {cid} should have {n} lines'
             for i in range(n):
                 o[f'ScenarioText{i+1}']['v'] = scenario[i]
+            audio_file = os.path.join(self.audio, o['ScenarioAudio']['v'] + '.ogg')
+            if os.path.isfile(audio_file):
+                self._set_scenario_audio_cue(o, audio_file)
+                copy_file(audio_file, os.path.join(path_folder, cid, o['ScenarioAudio']['v'] + '.ogg'))
             write_to_folder(cid, path_folder, o)
         return obj
 
@@ -95,3 +112,108 @@ class SplitTheRoom(Game):
     @decode_mapping(PATH + r'\content\SplitTheRoomFinal.jet', build + 'scenarios_final.json', PATH + r'\content\SplitTheRoomFinal.jet')
     def decode_scenarios_final(self, obj, trans):
         return self._decode_scenario(obj, trans, PATH + r'\content\SplitTheRoomFinal')
+
+    @decode_mapping(folder + 'audio_subtitles.json', build + 'audio_subtitles.json', out=False)
+    def upload_audio(self, original, obj):
+        d = Drive(self.drive)
+        data = []
+        for cid in tqdm.tqdm(obj):
+            ogg = f'{cid}.ogg'
+            data.append({'id': cid, 'ogg': ogg, 'text': obj[cid]['text'].strip().replace('\n', ' '),
+                         'context': obj[cid]['crowdinContext'],
+                         'original': original[cid]['text'].strip().replace('\n', ' ')})
+            d.upload(PATH_MEDIA, ogg)
+        for i in data:
+            i['link'] = d.get_link(i['ogg'])
+        pd.DataFrame(data).to_csv(self.folder + 'audio.tsv', sep='\t', encoding='utf8', index=False)
+
+    @decode_mapping(build + 'scenarios.json', build + 'scenarios_later.json', out=False)
+    def upload_audio_categories(self, round1, round2):
+        d = Drive(self.drive)
+        data = []
+        for cid in tqdm.tqdm(round1):
+            o = read_from_folder(cid, PATH + r'\content\SplitTheRoomShortie')
+            ogg = f"{o['CategoryAudio']['v']}.ogg"
+            data.append({'id': cid, 'ogg': ogg, 'text': round1[cid]['category']['text'], 'original': o['CategoryAudio']['s']})
+            d.upload(PATH + rf'\content\SplitTheRoomShortie\{cid}', ogg)
+        for cid in tqdm.tqdm(round2):
+            o = read_from_folder(cid, PATH + r'\content\SplitTheRoomLater')
+            ogg = f"{o['CategoryAudio']['v']}.ogg"
+            data.append({'id': cid, 'ogg': ogg, 'text': round2[cid]['category']['text'], 'original': o['CategoryAudio']['s']})
+            d.upload(PATH + rf'\content\SplitTheRoomLater\{cid}', ogg)
+        for i in data:
+            i['link'] = d.get_link(i['ogg'])
+        pd.DataFrame(data).to_csv(self.folder + 'audio_categories.tsv', sep='\t', encoding='utf8', index=False)
+
+    @decode_mapping(build + 'scenarios.json', build + 'scenarios_later.json', out=False)
+    def upload_audio_categories(self, round1, round2):
+        d = Drive(self.drive)
+        data = []
+        for cid in tqdm.tqdm(round1):
+            o = read_from_folder(cid, PATH + r'\content\SplitTheRoomShortie')
+            ogg = f"{o['CategoryAudio']['v']}.ogg"
+            data.append({'id': cid, 'ogg': ogg, 'text': round1[cid]['category']['text'], 'original': o['CategoryAudio']['s']})
+            d.upload(PATH + rf'\content\SplitTheRoomShortie\{cid}', ogg)
+        for cid in tqdm.tqdm(round2):
+            o = read_from_folder(cid, PATH + r'\content\SplitTheRoomLater')
+            ogg = f"{o['CategoryAudio']['v']}.ogg"
+            data.append({'id': cid, 'ogg': ogg, 'text': round2[cid]['category']['text'], 'original': o['CategoryAudio']['s']})
+            d.upload(PATH + rf'\content\SplitTheRoomLater\{cid}', ogg)
+        for i in data:
+            i['link'] = d.get_link(i['ogg'])
+        pd.DataFrame(data).to_csv(self.folder + 'audio_categories.tsv', sep='\t', encoding='utf8', index=False)
+
+    @decode_mapping(build + 'scenarios.json', build + 'scenarios_later.json', build + 'scenarios_final.json', out=False)
+    def upload_audio_scenarios(self, round1, round2, final):
+        d = Drive(self.drive)
+        data = []
+        for round_num, round_name in self.rounds.items():
+            scenarios = {1: round1, 2: round2, 3: final}[round_num]
+            for cid in tqdm.tqdm(scenarios):
+                o = read_from_folder(cid, PATH + rf'\content\{round_name}')
+                ogg = f"{o['ScenarioAudio']['v']}.ogg"
+                text = scenarios[cid]['scenario']['text'] + '\n' + scenarios[cid]['questionText']['text']
+                data.append({'id': cid, 'ogg': ogg, 'original': o['ScenarioAudio']['s'], 'text': text})
+                d.upload(PATH + rf'\content\{round_name}\{cid}', ogg)
+        for i in data:
+            i['link'] = d.get_link(i['ogg'])
+        pd.DataFrame(data).to_csv(self.folder + 'audio_scenarios.tsv', sep='\t', encoding='utf8', index=False, quoting=csv.QUOTE_ALL)
+
+    @decode_mapping(build + 'scenarios_final.json', out=False)
+    def upload_audio_responses(self, final):
+        d = Drive(self.drive)
+        data = []
+        for cid in tqdm.tqdm(final):
+            o = read_from_folder(cid, PATH + rf'\content\SplitTheRoomFinal')
+            if o['HasResponseAudio']['v'] != 'true':
+                continue
+            ogg = f"{o['ResponseAudio']['v']}.ogg"
+            text = final[cid]['response']['text']
+            data.append({'id': cid, 'ogg': ogg, 'original': o['ResponseAudio']['s'], 'text': text})
+            d.upload(PATH + rf'\content\SplitTheRoomFinal\{cid}', ogg)
+        for i in data:
+            i['link'] = d.get_link(i['ogg'])
+        pd.DataFrame(data).to_csv(self.folder + 'audio_responses.tsv', sep='\t', encoding='utf8')
+
+    @decode_mapping(build + 'scenarios.json', build + 'scenarios_later.json', out=False)
+    def _copy_audio_to_scenarios(self, round1, round2):
+        for cid in tqdm.tqdm(round1):
+            o = read_from_folder(cid, PATH + r'\content\SplitTheRoomShortie')
+            ogg = f"{o['CategoryAudio']['v']}.ogg"
+            copy_file(os.path.join(self.audio, ogg), os.path.join(PATH, 'content', 'SplitTheRoomShortie', str(cid), ogg))
+        for cid in tqdm.tqdm(round2):
+            o = read_from_folder(cid, PATH + r'\content\SplitTheRoomLater')
+            ogg = f"{o['CategoryAudio']['v']}.ogg"
+            copy_file(os.path.join(self.audio, ogg), os.path.join(PATH, 'content', 'SplitTheRoomLater', str(cid), ogg))
+
+    @staticmethod
+    def _set_scenario_audio_cue(obj: dict, audio_path: str):
+        markers = get_audio_markers(audio_path)
+        n = len([k for k in obj if k.startswith('$ScenarioAudio$ScenarioTextCue')])
+        assert len(markers) == n, f'Audio {audio_path} should have {n} markers, but has only {len(markers)}'
+        for i in range(2, 10):
+            key = f'$ScenarioAudio$ScenarioTextCue{i}'
+            if key not in obj:
+                break
+            obj[key]['v'] = markers.pop(0)
+        obj['$ScenarioAudio$QuestionTextCue']['v'] = markers.pop(0)
