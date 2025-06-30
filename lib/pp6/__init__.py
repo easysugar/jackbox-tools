2d133d1c4cb14a616d83d1c191b25b091624b6c2
easysugar <suprunov.vladyslav@gmail.com>
2025-06-30 12:02:46 +0300

    use gamepack in jpp6

--- lib/pp6/__init__.py
+++ lib/pp6/__init__.py
@@ -1,3 +1,2 @@
-from .jokeboat import Jokeboat
 from .jpp6 import JPP6
 from .old_tmp2 import OldTMP2
--- lib/pp6/jpp6.py
+++ lib/pp6/jpp6.py
@@ -1,21 +1,16 @@
 from datetime import datetime
 
-from lib.game import Game
 from paths import JPP6_PATH, JPP6_RELEASE_PATH
-from .old_tmp2 import OldTMP2
-
-PATH_GAME = JPP6_PATH
-PATH_RELEASE = JPP6_RELEASE_PATH
-INSTALL_DATE = datetime(2024, 10, 26)
+from ..pack import GamePack
 
 
-class JPP6(Game):
-    def decode_all(self):
-        OldTMP2().decode_all()
-        self.update_localization(rf'{PATH_GAME}\Localization.json', '../build/uk/JPP6/localization.json')
-        self.update_localization(rf'{PATH_GAME}\games\Picker\Localization.json', '../build/uk/JPP6/localization_pack.json')
-
-    def release(self):
-        self.decode_all()
-        self.copy_to_release(PATH_GAME, PATH_RELEASE, INSTALL_DATE)
-        self.make_archive(PATH_RELEASE, 'JPP6-UA.zip')
+class JPP6(GamePack):
+    path_game = JPP6_PATH
+    path_release = JPP6_RELEASE_PATH
+    install_date = datetime(2025, 1, 1)
+    release_name = 'JPP6-UA.zip'
+    games = []  # OldTMP2
+    localizations = {
+        '../build/uk/JPP6/localization.json': ['Localization.json'],
+        '../build/uk/JPP6/localization_pack.json': ['games/Picker/Localization.json'],
+    }
