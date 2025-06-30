4d8a80620dee247e18ffa2ef36205eb78ccf97a6
easysugar <suprunov.vladyslav@gmail.com>
2025-06-30 12:58:00 +0300

    rewrite jpp5 using game pack

--- lib/pp5/jpp5.py
+++ lib/pp5/jpp5.py
@@ -1,25 +1,17 @@
 from datetime import datetime
 
-from .split import SplitTheRoom
-from lib.game import Game
 from paths import JPP5_PATH, JPP5_RELEASE_PATH
-
-PATH_GAME = JPP5_PATH
-PATH_RELEASE = JPP5_RELEASE_PATH
-INSTALL_TIME = datetime(2024, 10, 13, 20, 10)
+from .split import SplitTheRoom
+from ..pack import GamePack
 
 
-class JPP5(Game):
-    def encode_all(self):
-        self._encode_localization(rf'{PATH_GAME}\Localization.json', '../data/pp5/localization.json')
-        self._encode_localization(rf'{PATH_GAME}\games\Picker\Localization.json', '../data/pp5/localization_pack.json')
-
-    def decode_all(self):
-        self.update_localization(rf'{PATH_GAME}\Localization.json', '../build/uk/JPP5/localization.json')
-        self.update_localization(rf'{PATH_GAME}\games\Picker\Localization.json', '../build/uk/JPP5/localization_pack.json')
-        SplitTheRoom().decode_all()
-
-    def release(self):
-        self.decode_all()
-        self.copy_to_release(PATH_GAME, PATH_RELEASE, INSTALL_TIME)
-        self.make_archive(PATH_RELEASE, 'JPP5-ua.zip')
+class JPP5(GamePack):
+    path_game = JPP5_PATH
+    path_release = JPP5_RELEASE_PATH
+    install_date = datetime(2024, 10, 13, 20, 10)
+    release_name = 'JPP5-ua.zip'
+    games = [SplitTheRoom]
+    localizations = {
+        '../build/uk/JPP5/localization.json': ['Localization.json'],
+        '../build/uk/JPP5/localization_pack.json': ['games/Picker/Localization.json'],
+    }
