2c5df0f2d555c91dcbee952970cdb9f9ce1c4776
easysugar <suprunov.vladyslav@gmail.com>
2025-07-18 20:43:51 +0300

    use gamepack module for jpp7

--- lib/pp7/jpp7.py
+++ lib/pp7/jpp7.py
@@ -1,19 +1,18 @@
 from datetime import datetime
 
-from lib.game import Game
+from paths import JPP7_PATH, JPP7_RELEASE_PATH
+from .old_quiplash3 import OldQuiplash3
+from .talks import Talks
+from ..pack import GamePack
 
-PATH_GAME = r'C:\Program Files (x86)\Steam\steamapps\common\The Jackbox Party Pack 7'
-PATH_RELEASE = r'C:\Users\админ\Desktop\Jackbox\games\jpp7\jpp7'
-INSTALL_DATE = datetime(2024, 2, 5)
 
-
-class JPP7(Game):
-    def decode_all(self):
-        OldQuiplash3().decode_all()
-        self.update_localization(rf'{PATH_GAME}\Localization.json', '../build/uk/JPP7/localization.json')
-        self.update_localization(rf'{PATH_GAME}\games\PartyPack\Localization.json', '../build/uk/JPP7/localization_pack.json')
-
-    def release(self):
-        self.decode_all()
-        self.copy_to_release(PATH_GAME, PATH_RELEASE, INSTALL_DATE)
-        self.make_archive(PATH_RELEASE, 'JPP7-UA.zip')
+class JPP7(GamePack):
+    path_game = JPP7_PATH
+    path_release = JPP7_RELEASE_PATH
+    install_date = datetime(2025, 1, 1)
+    release_name = 'JPP7-UA.zip'
+    games = [OldQuiplash3, Talks]
+    localizations = {
+        '../build/uk/JPP7/localization.json': ['Localization.json'],
+        '../build/uk/JPP7/localization_pack.json': ['games/Picker/Localization.json'],
+    }
