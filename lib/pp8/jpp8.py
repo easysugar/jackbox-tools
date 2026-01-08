70bf083de0c1e6117ffd88fb4b9064b4b7383781
easysugar <suprunov.vladyslav@gmail.com>
2026-01-08 22:52:40 +0200

    add pollmine to jpp8

--- lib/pp8/jpp8.py
+++ lib/pp8/jpp8.py
@@ -2,8 +2,9 @@ from datetime import datetime
 
 from lib.pack import GamePack
 from paths import JPP8_PATH, JPP8_RELEASE_PATH
-from .jobjob import JobJob
 from .drawful3 import Drawful3
+from .jobjob import JobJob
+from .. import PollMine
 
 
 class JPP8(GamePack):
@@ -11,7 +12,7 @@ class JPP8(GamePack):
     path_release = JPP8_RELEASE_PATH
     install_date = datetime(2025, 5, 20)
     release_name = 'JPP8-UA.zip'
-    games = [JobJob, Drawful3]
+    games = [JobJob, Drawful3, PollMine]
     localizations = {
         '../build/uk/JPP8/localization.json': ['Localization.json'],
         '../build/uk/JPP8/localization_pack.json': ['games/Picker/Localization.json'],
