d028cf9e49a9dbaefd972ff3745c09353603df87
easysugar <suprunov.vladyslav@gmail.com>
2026-01-14 22:42:25 +0200

    add oldtmp2 to jpp6

--- lib/pp6/jpp6.py
+++ lib/pp6/jpp6.py
@@ -1,6 +1,7 @@
 from datetime import datetime
 
 from paths import JPP6_PATH, JPP6_RELEASE_PATH
+from .old_tmp2 import OldTMP2
 from ..pack import GamePack
 
 
@@ -9,7 +10,7 @@ class JPP6(GamePack):
     path_release = JPP6_RELEASE_PATH
     install_date = datetime(2025, 1, 1)
     release_name = 'JPP6-UA.zip'
-    games = []  # OldTMP2
+    games = [OldTMP2]
     localizations = {
         '../build/uk/JPP6/localization.json': ['Localization.json'],
         '../build/uk/JPP6/localization_pack.json': ['games/Picker/Localization.json'],
