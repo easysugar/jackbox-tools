7dacce65b760584740a2b4b355d503b4593949e5
easysugar <suprunov.vladyslav@gmail.com>
2026-01-22 22:47:36 +0200

    use drop_keys in count_strings_and_words

--- lib/utils.py
+++ lib/utils.py
@@ -1,13 +1,19 @@
-import functools
 import re
-from typing import Any, Tuple
+from typing import Any, Tuple, Optional
 
 
-def count_strings_and_words(obj: Any) -> Tuple[int, int]:
+def count_strings_and_words(obj: Any, drop_keys: Optional[tuple] = None) -> Tuple[int, int]:
     if isinstance(obj, str) and not obj.isdigit() and obj and not obj.isspace():
         return 1, len(re.findall(r'\S+', obj))
     if isinstance(obj, list):
-        return functools.reduce(lambda x, y: (x[0] + y[0], x[1] + y[1]), map(count_strings_and_words, obj), (0, 0))
+        strings, words = 0, 0
+        for item in obj:
+            x, y = count_strings_and_words(item, drop_keys)
+            strings += x
+            words += y
+        return strings, words
     if isinstance(obj, dict):
-        return count_strings_and_words(list(obj.values()))
+        if drop_keys:
+            obj = {key: value for key, value in obj.items() if key not in drop_keys}
+        return count_strings_and_words(list(obj.values()), drop_keys)
     return 0, 0
