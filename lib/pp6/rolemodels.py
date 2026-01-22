b6437c54064584dc6a20e18148e73b72e04f1447
easysugar <suprunov.vladyslav@gmail.com>
2026-01-22 23:03:06 +0200

    fix role models

--- lib/pp6/rolemodels.py
+++ lib/pp6/rolemodels.py
@@ -1,7 +1,6 @@
 import os
 
 from lib.game import Game, clean_text
-from lib.pp11.legends import drop_keys
 from lib.utils import count_strings_and_words
 from paths import JPP6_PATH
 
@@ -22,8 +21,8 @@ class RoleModels(Game):
         strings, words = count_strings_and_words([
             self._read_json(os.path.join(self.game, 'Localization.json'))['table']['en'],
             audios,
-            drop_keys(self.read_jet('DataAnalysis'), 'contradiction', 'opposite', 'same'),
-            drop_keys(self.read_jet('PopCulturePrompt'), 'tags', 'id'),
-            drop_keys(self.read_jet('SituationalPrompt'), 'tags', 'id'),
-        ])
+            self.read_jet('DataAnalysis'),
+            self.read_jet('PopCulturePrompt'),
+            self.read_jet('SituationalPrompt'),
+        ], ('contradiction', 'opposite', 'same', 'id', 'tags'))
         print(f'Total strings: {strings}\nTotal words: {words}')
