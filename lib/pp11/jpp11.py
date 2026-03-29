c19b70d1daa863d6b46b769db62d67d4e674a559
easysugar <suprunov.vladyslav@gmail.com>
2026-03-29 19:23:38 +0300

    fix jpp11

--- lib/pp11/jpp11.py
+++ lib/pp11/jpp11.py
@@ -2,13 +2,13 @@ from datetime import datetime
 
 from lib.pack import GamePack
 from paths import JPP11_PATH, JPP11_RELEASE_PATH
-from .. import HearSay
+from .hearsay import HearSay
 
 
 class JPP11(GamePack):
     path_game = JPP11_PATH
     path_release = JPP11_RELEASE_PATH
-    install_date = datetime(2025, 3, 24)
+    install_date = datetime(2026, 3, 22)
     release_name = 'JPP11-UA.zip'
     games = [HearSay]
     localizations = {
