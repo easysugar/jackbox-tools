865738ad1cf6ff7871b6fe212342b51a6a1bfc61
easysugar <suprunov.vladyslav@gmail.com>
2026-06-06 16:38:05 +0300

    add encodes to wheel

--- lib/pp8/thewheelofenormousproportions.py
+++ lib/pp8/thewheelofenormousproportions.py
@@ -56,3 +56,38 @@ class TheWheelOfEnormousProportions(Game):
             text = build_text(prompt, prompt_header, header, answers)
             result[cid] = text
         self.write_to_data('matching.json', result)
+
+    def encode_rapid_fire(self):
+        obj = self.read_jet('RapidFire')
+        result = {}
+        for c in obj['content']:
+            cxt = {'crowdinContext': self.get_context(c, c['sort'], c['subtype'])}
+            cid = c['id']
+            prompt = c['prompt']
+            unit = c['unit'].strip()
+            if unit:
+                prompt = prompt + ' ' + unit
+                assert unit.startswith('(') and unit.endswith(')')
+            c['answers'].sort(key=lambda x: float(x['value']))
+            answers = [_['text'] for _ in c['answers']]
+            if c['subtype'] in ('most/least', 'closest') and c['sort'] == 'ascending':
+                answers = answers[::-1]
+            text = build_text(prompt, answers)
+            result[cid] = {'text': text, **cxt}
+        self.write_to_data('rapid_fire.json', result)
+
+    def encode_typing_list(self):
+        obj = self.read_jet('TypingList')
+        result = {}
+        for c in obj['content']:
+            cxt = {'crowdinContext': self.get_context(c, c['subtype'], 'exact spelling' if c['exactSpelling'] else '')}
+            cid = c['id']
+            prompt = c['prompt']
+            answers = []
+            for ans in c['answers']:
+                spellings = [ans['text'], *ans.get('altSpellings', [])]
+                spellings = [_.replace(',', '').strip() for _ in spellings]
+                answers.append(', '.join(spellings))
+            text = build_text(prompt, answers)
+            result[cid] = {'text': text, **cxt}
+        self.write_to_data('typing_list.json', result)
